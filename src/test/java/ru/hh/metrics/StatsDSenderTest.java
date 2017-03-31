9bb5ac689a54bad89874d1fe79ca24a904cc1417
Anton Ivanov <an.ivanov@hh.ru>
2017-03-31 14:33:54 +0300

    HH-68028 add Max

--- src/test/java/ru/hh/metrics/StatsDSenderTest.java
+++ src/test/java/ru/hh/metrics/StatsDSenderTest.java
@@ -6,26 +6,39 @@ import static org.junit.Assert.assertEquals;
 
 public class StatsDSenderTest {
   @Test
+  public void testGetTagStringNullTags() {
+    assertEquals("metricName", StatsDSender.getFullMetricName("metricName", null));
+  }
+
+  @Test
   public void testGetTagStringNoTags() {
-    assertEquals("", StatsDSender.getTagString(new Tag[]{}));
+    assertEquals("metricName", StatsDSender.getFullMetricName("metricName", new Tag[]{}));
   }
 
   @Test
   public void testGetTagStringOneTag() {
-    assertEquals("label_is_right", StatsDSender.getTagString(new Tag[]{new Tag("label", "right")}));
+    assertEquals("metricName.label_is_right", StatsDSender.getFullMetricName("metricName", new Tag[]{new Tag("label", "right")}));
   }
 
   @Test
   public void testGetTagStringTwoTags() {
-    assertEquals("label_is_right.answer_is_42", StatsDSender.getTagString(
+    assertEquals(
+        "metricName.label_is_right.answer_is_42",
+        StatsDSender.getFullMetricName(
+            "metricName",
             new Tag[]{new Tag("label", "right"), new Tag("answer", "42")}
-    ));
+        )
+    );
   }
 
   @Test
   public void testGetTagStringTwoTagsAnotherOrder() {
-    assertEquals("answer_is_42.label_is_right", StatsDSender.getTagString(
+    assertEquals(
+        "metricName.answer_is_42.label_is_right",
+        StatsDSender.getFullMetricName(
+            "metricName",
             new Tag[]{new Tag("answer", "42"), new Tag("label", "right")}
-    ));
+        )
+    );
   }
 }
